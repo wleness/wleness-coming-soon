a63b77f0fe96f79693e9bbe0525c556af6f6ebdf
Omprakash Prajapati <oprajapati249@gmail.com>
2023-07-17 22:51:36 +0530

    updated ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 export default {
   content: ["./*.html"],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1720px',
+    },
     extend: {
       fontFamily: {
-        'poppins': "'Poppins', sans-serif",
+        'lato': ['Lato', 'sans-serif'],
     },
   },
   plugins: [],
